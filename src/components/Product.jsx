2ad78a36dcf6a901301e2059d8d0e7f24450d203
someshshenoy5 <shenoysomesh5@gmail.com>
2024-04-24 17:27:21 +0530

    Initial commit

---
+++ src/components/Product.jsx
@@ -0,0 +1,93 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { useState } from "react";
+import AppContext from "../Context/Context";
+import axios from "../axois";
+const Product = () => {
+  const { id } = useParams();
+  const { data } = useContext(AppContext);
+  const [product, setProduct] = useState(null);
+  const [imageUrl, setImageUrl] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/api/product/${id}`
+        );
+        setProduct(response.data);
+        if (response.data.imageName) {
+          fetchImage();
+          console.log(response.data.imageName);
+        }
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      }
+    };
+
+    const fetchImage = async () => {
+      const response = await axios.get(
+        `http://localhost:8080/api/product/${id}/image`,
+        { responseType: "blob" }
+      );
+      setImageUrl(URL.createObjectURL(response.data));
+      console.log(response.data);
+    };
+
+    fetchProduct();
+  }, [id]);
+
+  console.log("Product ID:", id);
+
+
+
+  if (!product) {
+    return (
+      <h2 className="text-center" style={{ padding: "10rem" }}>
+        Loading...
+      </h2>
+    );
+  }
+  return (
+    <>
+      <div className="containers">
+        {/* <div className="left-column-img"> */}
+        <img
+          className="left-column-img"
+          src={imageUrl}
+          alt={product.imageName}
+        />
+
+        {/* </div> */}
+        <div className="right-column">
+          <div className="product-description">
+            <span>{product.category}</span>
+            <h1>{product.name}</h1>
+            <h5>{product.brand}</h5>
+            <p>{product.description}</p>
+          </div>
+
+          <div className="product-price">
+            <span>{"$" + product.price}</span>
+            <a href="#" className="cart-btn">
+              Add to cart
+            </a>
+            <h6>
+              Stock Available :{" "}
+              <i style={{ color: "green", fontWeight: "bold" }}>
+                {product.stockQuantity}
+              </i>
+            </h6>
+            <p className="release-date">
+              <h6>Product listed on:</h6>
+              <i> {new Date(product.releaseDate).toLocaleDateString()}</i>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Product;
